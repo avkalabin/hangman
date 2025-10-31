204f9f55794481e69706891de89106ebf55890bb
avkalabin <a.kalabin@inbox.ru>
2025-10-31 18:55:29 +0500

    add words list and pick random

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,15 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        List<String> words = Files.readAllLines(Path.of("src/main/resources/dictionary.txt"));
+        System.out.println(words.size() + " слов");
+        Random random = new Random();
         while (true) {
             System.out.println("[N]ew game or [E]xit?");
             Scanner in = new Scanner(System.in);
@@ -11,6 +19,9 @@ public class Main {
                 break;
             } else if ("n".equalsIgnoreCase(input)) {
                 System.out.println("Start");
+                int randomIndex = random.nextInt(words.size());
+                String word = words.get(randomIndex);
+                System.out.println(word);
 
             }
         }
