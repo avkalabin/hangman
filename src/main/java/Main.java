255c11893af621c9bf94a1abfbae6eab3669a0c9
69KalabinAV <KalabinAV@utgard.cbr.ru>
2025-11-01 18:03:39 +0500

    add letter verification

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,28 +1,50 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
+        int HANGMAN_SIZE = 6;
         List<String> words = Files.readAllLines(Path.of("src/main/resources/dictionary.txt"));
         System.out.println(words.size() + " слов");
         Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.println("[N]ew game or [E]xit?");
-            Scanner in = new Scanner(System.in);
-            String input = in.nextLine();
-            if ("e".equalsIgnoreCase(input)) {
+            Character newGameOrExit = scanner.next().charAt(0);
+            if ("e".equalsIgnoreCase(String.valueOf(newGameOrExit))) {
                 System.out.println("Exit...");
                 break;
-            } else if ("n".equalsIgnoreCase(input)) {
-                System.out.println("Start");
+            } else if ("n".equalsIgnoreCase(String.valueOf(newGameOrExit))) {
                 int randomIndex = random.nextInt(words.size());
-                String word = words.get(randomIndex);
+                var word = words.get(randomIndex);
                 System.out.println(word);
-
+                StringBuilder maskedWord = new StringBuilder();
+                maskedWord.append("*".repeat(word.length()));
+                System.out.println(maskedWord);
+                int fail = 0;
+                var usedLetters = new ArrayList<Character>();
+                while (fail < HANGMAN_SIZE) {
+                    System.out.println("Введите букву: ");
+                    char letter = scanner.next().charAt(0);
+                    if (word.contains(String.valueOf(letter)) && !usedLetters.contains(letter)) {
+                        for (int i = 0; i < word.length(); i++) {
+                            if (word.charAt(i) == letter) {
+                                maskedWord.replace(i, i, String.valueOf(letter));
+                            }
+                            }
+                        } else {
+                        fail++;
+                    }
+                    System.out.println(maskedWord);
+                    usedLetters.add(letter);
+                    System.out.println("Использованы буквы: " + usedLetters);
+                }
             }
         }
     }
