307d72f8b5f6885f1cf910dbc300c08833b56442
avkalabin <a.kalabin@inbox.ru>
2025-11-02 20:27:20 +0500

    Refactor: extract playRound method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,9 +7,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         final List<String> wordsList = Files.readAllLines(Path.of("src/main/resources/dictionary.txt"));
-        final String LOWERCASE_RUSSIAN_LETTERS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
         System.out.println(wordsList.size() + " слов");
-        Random random = new Random();
         Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.println("[N]ew game or [E]xit?");
@@ -18,47 +16,58 @@ public class Main {
                 System.out.println("Exit...");
                 break;
             } else if ("n".equalsIgnoreCase(String.valueOf(newGameOrExit))) {
-                final int randomIndex = random.nextInt(wordsList.size());
-                final var hiddenWord = wordsList.get(randomIndex).toLowerCase();
-                StringBuilder maskedWord = new StringBuilder();
-                int attemptCount = 6;
-                maskedWord.append("*".repeat(hiddenWord.length()));
-                drawHangman(attemptCount);
-                System.out.println(maskedWord);
-                var usedLetters = new HashSet<Character>();
-                while (attemptCount > 0) {
-                    System.out.println("Введите букву: ");
-                    final char letter = Character.toLowerCase(scanner.next().charAt(0));
-                    if (!LOWERCASE_RUSSIAN_LETTERS.contains(String.valueOf(letter))) {
-                        continue;
-                    }
-                    if (hiddenWord.contains(String.valueOf(letter)) && !usedLetters.contains(letter)) {
-                        for (int i = 0; i < hiddenWord.length(); i++) {
-                            if (hiddenWord.charAt(i) == letter) {
-                                maskedWord.replace(i, i + 1, String.valueOf(letter));
-                            }
-                        }
-                    } else if (!hiddenWord.contains(String.valueOf(letter)) && !usedLetters.contains(letter)) {
-                        attemptCount--;
-                        System.out.println("Такой буквы нет");
-                        if (attemptCount > 0) {
-                            System.out.println("У вас осталось " + attemptCount + " попыток");
-                        } else {
-                            System.out.println("Вы ПРОИГРАЛИ");
-                            System.out.println("Загаданное слово было: " + hiddenWord);
-                            drawHangman(attemptCount);
-                            break;
-                        }
-                    }
-                    drawHangman(attemptCount);
-                    System.out.println(maskedWord);
-                    usedLetters.add(letter);
-                    System.out.println("Использованы буквы: " + usedLetters + "\n");
-                    if (!maskedWord.toString().contains("*")) {
-                        System.out.println("ПОБЕДА!!!");
-                        break;
+                playRound(wordsList, scanner);
+            }
+        }
+    }
+
+    private static void playRound(List<String> wordsList, Scanner scanner) {
+        final String RUSSIAN_LETTERS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+        Random random = new Random();
+        final int randomIndex = random.nextInt(wordsList.size());
+        final var hiddenWord = wordsList.get(randomIndex).toLowerCase();
+        StringBuilder maskedWord = new StringBuilder();
+        int attemptCount = 6;
+        maskedWord.append("*".repeat(hiddenWord.length()));
+        drawHangman(attemptCount);
+        System.out.println(maskedWord);
+        var usedLetters = new HashSet<Character>();
+        while (true) {
+            System.out.println("Введите букву: ");
+            final char letter = Character.toLowerCase(scanner.next().charAt(0));
+            if (!RUSSIAN_LETTERS.contains(String.valueOf(letter))) {
+                System.out.println("Должна быть введена русская буква");
+                continue;
+            }
+            if (usedLetters.contains(letter)) {
+                System.out.println("Это букву уже вводили");
+                continue;
+            }
+            if (hiddenWord.contains(String.valueOf(letter))) {
+                for (int i = 0; i < hiddenWord.length(); i++) {
+                    if (hiddenWord.charAt(i) == letter) {
+                        maskedWord.setCharAt(i, letter);
                     }
                 }
+            } else {
+                attemptCount--;
+                System.out.println("Такой буквы нет");
+                if (attemptCount > 0) {
+                    System.out.println("У вас осталось " + attemptCount + " попыток");
+                } else {
+                    drawHangman(attemptCount);
+                    System.out.println("Вы ПРОИГРАЛИ");
+                    System.out.println("Загаданное слово было: " + hiddenWord);
+                    break;
+                }
+            }
+            drawHangman(attemptCount);
+            System.out.println(maskedWord);
+            usedLetters.add(letter);
+            System.out.println("Использованы буквы: " + usedLetters + "\n");
+            if (!maskedWord.toString().contains("*")) {
+                System.out.println("ПОБЕДА!!!");
+                break;
             }
         }
     }
